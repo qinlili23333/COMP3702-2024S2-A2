9d09ad6320317b25bec0a8f78d917b1c97f0f048
琴梨梨OvO <24567775+qinlili23333@users.noreply.github.com>
2024-09-07 09:01:45 +1000

    1

---
+++ solution.py
@@ -0,0 +1,182 @@
+import sys
+import time
+from constants import *
+from environment import *
+from state import State
+"""
+solution.py
+
+This file is a template you should use to implement your solution.
+
+You should implement each section below which contains a TODO comment.
+
+COMP3702 2022 Assignment 2 Support Code
+
+"""
+
+
+class Solver:
+
+    def __init__(self, environment: Environment):
+        self.environment = environment
+        #
+        # TODO: Define any class instance variables you require (e.g. dictionary mapping state to VI value) here.
+        #
+        pass
+
+    @staticmethod
+    def testcases_to_attempt():
+        """
+        Return a list of testcase numbers you want your solution to be evaluated for.
+        """
+        # TODO: modify below if desired (e.g. disable larger testcases if you're having problems with RAM usage, etc)
+        return [1, 2, 3, 4, 5, 6]
+
+    # === Value Iteration ==============================================================================================
+
+    def vi_initialise(self):
+        """
+        Initialise any variables required before the start of Value Iteration.
+        """
+        #
+        # TODO: Implement any initialisation for Value Iteration (e.g. building a list of states) here. You should not
+        #  perform value iteration in this method.
+        #
+        # In order to ensure compatibility with tester, you should avoid adding additional arguments to this function.
+        #
+        pass
+
+    def vi_is_converged(self):
+        """
+        Check if Value Iteration has reached convergence.
+        :return: True if converged, False otherwise
+        """
+        #
+        # TODO: Implement code to check if Value Iteration has reached convergence here.
+        #
+        # In order to ensure compatibility with tester, you should avoid adding additional arguments to this function.
+        #
+        pass
+
+    def vi_iteration(self):
+        """
+        Perform a single iteration of Value Iteration (i.e. loop over the state space once).
+        """
+        #
+        # TODO: Implement code to perform a single iteration of Value Iteration here.
+        #
+        # In order to ensure compatibility with tester, you should avoid adding additional arguments to this function.
+        #
+        pass
+
+    def vi_plan_offline(self):
+        """
+        Plan using Value Iteration.
+        """
+        # !!! In order to ensure compatibility with tester, you should not modify this method !!!
+        self.vi_initialise()
+        while True:
+            self.vi_iteration()
+
+            # NOTE: vi_iteration is always called before vi_is_converged
+            if self.vi_is_converged():
+                break
+
+    def vi_get_state_value(self, state: State):
+        """
+        Retrieve V(s) for the given state.
+        :param state: the current state
+        :return: V(s)
+        """
+        #
+        # TODO: Implement code to return the value V(s) for the given state (based on your stored VI values) here. If a
+        #  value for V(s) has not yet been computed, this function should return 0.
+        #
+        # In order to ensure compatibility with tester, you should avoid adding additional arguments to this function.
+        #
+        pass
+
+    def vi_select_action(self, state: State):
+        """
+        Retrieve the optimal action for the given state (based on values computed by Value Iteration).
+        :param state: the current state
+        :return: optimal action for the given state (element of ROBOT_ACTIONS)
+        """
+        #
+        # TODO: Implement code to return the optimal action for the given state (based on your stored VI values) here.
+        #
+        # In order to ensure compatibility with tester, you should avoid adding additional arguments to this function.
+        #
+        pass
+
+    # === Policy Iteration =============================================================================================
+
+    def pi_initialise(self):
+        """
+        Initialise any variables required before the start of Policy Iteration.
+        """
+        #
+        # TODO: Implement any initialisation for Policy Iteration (e.g. building a list of states) here. You should not
+        #  perform policy iteration in this method. You should assume an initial policy of always move FORWARDS.
+        #
+        # In order to ensure compatibility with tester, you should avoid adding additional arguments to this function.
+        #
+        pass
+
+    def pi_is_converged(self):
+        """
+        Check if Policy Iteration has reached convergence.
+        :return: True if converged, False otherwise
+        """
+        #
+        # TODO: Implement code to check if Policy Iteration has reached convergence here.
+        #
+        # In order to ensure compatibility with tester, you should avoid adding additional arguments to this function.
+        #
+        pass
+
+    def pi_iteration(self):
+        """
+        Perform a single iteration of Policy Iteration (i.e. perform one step of policy evaluation and one step of
+        policy improvement).
+        """
+        #
+        # TODO: Implement code to perform a single iteration of Policy Iteration (evaluation + improvement) here.
+        #
+        # In order to ensure compatibility with tester, you should avoid adding additional arguments to this function.
+        #
+        pass
+
+    def pi_plan_offline(self):
+        """
+        Plan using Policy Iteration.
+        """
+        # !!! In order to ensure compatibility with tester, you should not modify this method !!!
+        self.pi_initialise()
+        while True:
+            self.pi_iteration()
+
+            # NOTE: pi_iteration is always called before pi_is_converged
+            if self.pi_is_converged():
+                break
+
+    def pi_select_action(self, state: State):
+        """
+        Retrieve the optimal action for the given state (based on values computed by Value Iteration).
+        :param state: the current state
+        :return: optimal action for the given state (element of ROBOT_ACTIONS)
+        """
+        #
+        # TODO: Implement code to return the optimal action for the given state (based on your stored PI policy) here.
+        #
+        # In order to ensure compatibility with tester, you should avoid adding additional arguments to this function.
+        #
+        pass
+
+    # === Helper Methods ===============================================================================================
+    #
+    #
+    # TODO: Add any additional methods here
+    #
+    #
+
